5a7cdba71bac19c8bed0bfefdc2114a0595ccd61
Rachel <rachel.m.harrison8@gmail.com>
2019-01-09 10:16:53 -0500

    update about me

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -25,8 +25,18 @@ const AboutPage = (props, context) => {
         <div className="about-wrapper">
           <div className="about-content" style={{ color: textPrimary }}>
             <p>
-              Welcome to my site! I am a third year student in Software Engineering 
+              Welcome to my site! My name is Rachel and I am currently a third year student in Software Engineering at the Univeristy of Ottawa. <br />
+              <br />
+              On my site you'll find stuff in two main areas: <br />
+              1. <b>Software Development</b> Languages and tools I like, work experience, personal projects etc. <br />
+              2. <b>Website Building:</b> Examples of my work building custom websites for people using tools like WordPress and Squarespace
             </p>
+            <br/>
+            This semester (January-April 2019) I am working as a part-time developer at <a href="https://www.arioplatform.com/" target="_blank">Ario</a>. I completed a co-op placement with Ario last
+            semester and I loved working there so much I came back for more! Working for a start-up is such a rewarding experience because I'm able to touch so many parts of the project.
+            <br />
+            <br />
+            In previous semesters I've completed co-op placements at <a href="https://fokoretail.com/" target="_blank">Foko</a> and with the IT branch of the Canada Revenue Agency.
           </div>
         </div>
       </div>
